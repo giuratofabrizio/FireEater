f5b3a9f6f281565b6baf5c90c7915c3604022415
giuratofabrizio <giurato.fabrizio@iisgalvanimi.edu.it>
2023-09-28 08:58:33 +0000

    definitiva

--- Python/FireEater.py
+++ Python/FireEater.py
@@ -2,7 +2,7 @@ import Adafruit_DHT
 import telebot
 from datetime import datetime
 import json
-bot = telebot.TeleBot("6390606092:AAHVH-cAokxfvnMbgI_FktakmkkMHAr3kGU")
+bot = telebot.TeleBot("telegram token")
 DHT_SENSOR = Adafruit_DHT.AM2302
 DHT_PIN = 4
 verifica = True
@@ -17,13 +17,14 @@ def send_message(msg):
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
     
+    temperature = round(temperature, 2)
     if temperature < 24:
         verifica = True
 
     if humidity is not None and temperature is not None:
         print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
         if temperature >= 24 and verifica == True:
-            send_message("Fire Alert Attivato\nLuogo: Istituto di Istruzione Superiore Luigi Galvani, Via Gatti 14 (Niguarda, MI)\nAlle ore: {}".format(datetime.now()))
+            send_message("Fire Alert Attivato\nLuogo: Istituto di Istruzione Superiore Luigi Galvani, Via Gatti 14 (Niguarda, MI). \nLuogo e orario: {}. \nTemperatura: {}".format(datetime.now(), temperature))
             verifica = False
     else:
         print("Failed to retrieve data from humidity sensor")
